4758e38b8296db46266ed1f7413947ba0e731807
Abaidullah <abaid@sunztech.com>
2020-03-10 20:06:39 +0500

    almost done desktop screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
 import React from 'react';
 import logo from './logo.svg';
+import baby_european from './assets/baby_european.png'
+import logo_babypoom from './assets/logo-babypoom.png'
+import gender from './assets/gender.svg';
+// import gender from './assets/gender.png';
+import baby from './assets/baby.png';
+import stork from './assets/stork.png';
+import cubes from './assets/cubes.png';
+import right_arrow from './assets/right-arrow.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grad-bg">
+      <div className="row">
+        <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3"></div>
+        <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6" style={{ backgroundColor: 'white', marginTop: '70px', borderRadius: '5px' }}>
+          <center>
+            <img src={logo_babypoom} width="120" style={{ borderRadius: '50% 50% 0px 0px', padding: '10px', backgroundColor: 'white', position: 'relative', top: -30 }} />
+            <br />
+            <img src={baby_european} width="400" style={{ padding: '50px' }} />
+          </center>
+        </div>
+        <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3"></div>
+      </div>
+
+      {/* 2nd row */}
+      <div className="row" style={{ marginTop: '10px', paddingBottom: '10px', textAlign: 'center' }}>
+
+        <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3"></div>
+        <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6" >
+
+          <div className="row" style={{ borderBottom: '1px solid white', padding: '10px' }}>
+            <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+            <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2" style={{ color: 'white', padding: '5px' }}>
+              <span>Genre</span>
+              <div style={{ backgroundColor: '#93cbca', borderRadius: '5px' }}>
+                <img src={gender} style={{ width: '100%', padding: '10px' }} />
+              </div>
+            </div>
+            <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2" style={{ color: 'white', padding: '5px' }}>
+              <span>Apparence</span>
+              <div style={{ backgroundColor: '#93cbca', borderRadius: '5px' }}>
+                <img src={baby} style={{ width: '100%', padding: '10px' }} />
+              </div>
+            </div>
+            <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2" style={{ color: 'white', padding: '5px' }}>
+              <span>Naissance</span>
+              <div style={{ backgroundColor: '#93cbca', borderRadius: '5px' }}>
+                <img src={stork} style={{ width: '100%', padding: '10px' }} />
+              </div>
+            </div>
+            <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2" style={{ color: 'white', padding: '5px' }}>
+              <span>Prenom</span>
+              <div style={{ backgroundColor: '#93cbca', borderRadius: '5px' }}>
+                <img src={cubes} style={{ width: '100%', padding: '10px' }} />
+              </div>
+            </div>
+            <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2" style={{ color: 'white', padding: '5px' }}>
+              <div style={{ backgroundColor: '#f59c9a', borderRadius: '5px' }}>
+                <span>J'ai fini</span>
+                <img src={right_arrow} style={{ width: '100%', padding: '10px' }} />
+              </div>
+            </div>
+
+            <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+          </div>
+          {/* end row */}
+          <div style={{ textAlign: 'center', color: 'white', margin: '10px' }}>Babypoom.c'est quoi ?</div>
+        </div>
+        {/* end 6 col */}
+        <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3"></div>
+
+      </div>
+      {/* end row */}
+
     </div>
   );
 }
